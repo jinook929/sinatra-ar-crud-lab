0b2b569778b8906c778df3016a3184e93c85addf
jinook929 <jinook929@gmail.com>
2021-02-23 15:21:35 -0500

    commit tree 70ded3978a405c951392409ef37d8225a1cc8405

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,44 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
+    erb :main
   end
+
+  get '/articles' do
+    @articles = Article.all
+    erb :index
+  end
+
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    article = Article.create(params)
+    
+    redirect "/articles/#{article.id}"
+  end
+
+  get '/articles/:id' do
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  get '/articles/:id/edit' do
+    @article = Article.find(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    params.delete(:_method)
+    Article.update(params[:id], params)
+    @article = Article.find(params[:id])
+    erb :show
+  end
+
+  delete '/articles/:id' do
+    Article.find(params[:id]).destroy
+    @articles = Article.all
+    erb :index
+  end 
 end
